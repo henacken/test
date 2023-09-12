3b90602e2b32ea9051f8c00f0490df0570521271
henacken <masahiro0921br@gmail.com>
2023-09-13 00:49:01 +0900

    全機能追加終了

--- calc.js
+++ calc.js
@@ -1,7 +1,11 @@
-function calc() {
+function calc(number) {
     if(number % 15 === 0) {
         console.log("15の倍数です");
     } else if(number % 5 === 0) {
         console.log("5の倍数です");
+    } else if(number % 3 === 0) {
+        console.log("3の倍数です");
+    } else {
+        console.log(number);
     }
 }
