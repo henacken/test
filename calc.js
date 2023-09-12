25a5ad29d925c64d194a62de70e8d420f4e4629d
henacken <masahiro0921br@gmail.com>
2023-09-12 22:34:42 +0900

    5の倍数表示機能追加 #3

--- calc.js
+++ calc.js
@@ -1,3 +1,7 @@
-if (number % 3 === 15) {
-    console.log("15の倍数です");
+function calc() {
+    if(number % 15 === 0) {
+        console.log("15の倍数です");
+    } else if(number % 5 === 0) {
+        console.log("5の倍数です");
+    }
 }
